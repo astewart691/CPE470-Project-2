4a4a484acddf082589581ab96267c7d2057a04c5
Aarron Stewart <astewart@nevada.unr.edu>
2016-11-05 20:04:14 -0700

    Initioal setup of project2

---
+++ project2a.py
@@ -0,0 +1,50 @@
+'''
+File Name: Project2a.py
+Author: Aarron Stewart
+Class: CPE470
+Term: Fall 2016
+'''
+
+# import libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main( ):
+    # Initialize variables
+    deltaT = 1
+    robotCoordinates = []
+    robotTheta = []
+    targetCoordinates = []
+    targetTheta = []
+    velocity = 0
+    lmbda = 8.5  # used for lambda variable
+    robotMax = 50  # Max speed for the robot
+
+    #  target initialization
+
+    #  set target initial position
+
+    #  set velocity of the target
+
+    #  set initial heading of the target
+
+    ########################################
+    #  robot Initialization
+
+    #  initial position of the robot
+
+    #  initial velocity of the robot
+
+    #  initial heading of the robot
+
+    ########################################
+    #  determine the difference between the robot and the target
+
+    #  save the relative position of the robot and the target
+
+    #  save the relative velocity of the robot and the target
+
+    
+
+main( )
